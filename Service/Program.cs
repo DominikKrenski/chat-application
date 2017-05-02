dbd3d7eed04aab340d4e7a882c9767df6083f5c1
Dominik Kreński <dominik.krenski@gmail.com>
2017-05-02 18:41:57 +0200

    Zaimplementowano klasę startową serwisu

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(Implementations.Server)))
+            {
+                host.Open();
+                Console.WriteLine($"Host started at {DateTime.Now.ToString()}");
+                Console.WriteLine("Press enter to close the server");
+                Console.ReadKey();
+            }
         }
     }
 }
