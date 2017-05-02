0d4f50f891ddddf18de5d2f205f2d9581c938f93
Dominik Kreński <dominik.krenski@gmail.com>
2017-05-02 17:48:14 +0200

    Dodano usługę WCF

---
+++ Service/Implementations/Server.cs
@@ -0,0 +1,18 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „Server” w kodzie i pliku konfiguracji.
+    public class Server : IServer
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ Service/Interfaces/IServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę interfejsu „IServer” w kodzie i pliku konfiguracji.
+    [ServiceContract]
+    public interface IServer
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
