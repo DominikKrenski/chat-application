3c02604fcd08ff2f8473da8e0c19cdfd8ca98e2c
Dominik Kreński <dominik.krenski@gmail.com>
2017-05-02 18:32:26 +0200

    Dodano interfejs IServerCallback

---
+++ Service/Interfaces/IServerCallback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IServerCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void RegisterNotify(string message);
+    }
+}
