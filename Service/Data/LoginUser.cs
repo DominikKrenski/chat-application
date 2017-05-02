3735c2163c02b1f8e83ebcc7242f04e9eecfe055
Dominik Kreński <dominik.krenski@gmail.com>
2017-05-02 18:29:53 +0200

    Dodano kontrakty danych serwisu LoginUser oraz RegisterUser

---
+++ Service/Data/LoginUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Data
+{
+    [DataContract]
+    public class LoginUser
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
---
+++ Service/Data/RegisterUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Data
+{
+    [DataContract]
+    public class RegisterUser
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string PasswordConfirm { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string Sex { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
