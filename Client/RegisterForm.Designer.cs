c404516e2c748990764b37d7b88fbb11536a7cf2
Dominik Kreński <dominik.krenski@gmail.com>
2017-05-02 19:16:08 +0200

    Utworzono szablon formularza rejestracji

---
+++ Client/RegisterForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Client
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordConfirmLabel = new System.Windows.Forms.Label();
+            this.PasswordConfirmTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SurnameLabel = new System.Windows.Forms.Label();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.SexLabel = new System.Windows.Forms.Label();
+            this.SexComboBox = new System.Windows.Forms.ComboBox();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(12, 9);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(33, 13);
+            this.LoginLabel.TabIndex = 0;
+            this.LoginLabel.Text = "Login";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(15, 25);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(379, 20);
+            this.LoginTextBox.TabIndex = 1;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 57);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 2;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(15, 73);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(379, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // PasswordConfirmLabel
+            // 
+            this.PasswordConfirmLabel.AutoSize = true;
+            this.PasswordConfirmLabel.Location = new System.Drawing.Point(12, 105);
+            this.PasswordConfirmLabel.Name = "PasswordConfirmLabel";
+            this.PasswordConfirmLabel.Size = new System.Drawing.Size(114, 13);
+            this.PasswordConfirmLabel.TabIndex = 4;
+            this.PasswordConfirmLabel.Text = "Password Confirmation";
+            // 
+            // PasswordConfirmTextBox
+            // 
+            this.PasswordConfirmTextBox.Location = new System.Drawing.Point(15, 121);
+            this.PasswordConfirmTextBox.Name = "PasswordConfirmTextBox";
+            this.PasswordConfirmTextBox.Size = new System.Drawing.Size(379, 20);
+            this.PasswordConfirmTextBox.TabIndex = 5;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(12, 154);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 6;
+            this.NameLabel.Text = "Name";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(15, 170);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(379, 20);
+            this.NameTextBox.TabIndex = 7;
+            // 
+            // SurnameLabel
+            // 
+            this.SurnameLabel.AutoSize = true;
+            this.SurnameLabel.Location = new System.Drawing.Point(12, 202);
+            this.SurnameLabel.Name = "SurnameLabel";
+            this.SurnameLabel.Size = new System.Drawing.Size(49, 13);
+            this.SurnameLabel.TabIndex = 8;
+            this.SurnameLabel.Text = "Surname";
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(15, 218);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(379, 20);
+            this.SurnameTextBox.TabIndex = 9;
+            // 
+            // SexLabel
+            // 
+            this.SexLabel.AutoSize = true;
+            this.SexLabel.Location = new System.Drawing.Point(12, 250);
+            this.SexLabel.Name = "SexLabel";
+            this.SexLabel.Size = new System.Drawing.Size(25, 13);
+            this.SexLabel.TabIndex = 10;
+            this.SexLabel.Text = "Sex";
+            // 
+            // SexComboBox
+            // 
+            this.SexComboBox.FormattingEnabled = true;
+            this.SexComboBox.Location = new System.Drawing.Point(15, 266);
+            this.SexComboBox.Name = "SexComboBox";
+            this.SexComboBox.Size = new System.Drawing.Size(121, 21);
+            this.SexComboBox.TabIndex = 11;
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Location = new System.Drawing.Point(12, 299);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(26, 13);
+            this.AgeLabel.TabIndex = 12;
+            this.AgeLabel.Text = "Age";
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(15, 315);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(379, 20);
+            this.AgeTextBox.TabIndex = 13;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(160, 355);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(105, 23);
+            this.RegisterButton.TabIndex = 14;
+            this.RegisterButton.Text = "Create Account";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 390);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.AgeTextBox);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.SexComboBox);
+            this.Controls.Add(this.SexLabel);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.SurnameLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.PasswordConfirmTextBox);
+            this.Controls.Add(this.PasswordConfirmLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.LoginLabel);
+            this.Name = "RegisterForm";
+            this.Text = "RegisterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label PasswordConfirmLabel;
+        private System.Windows.Forms.TextBox PasswordConfirmTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label SurnameLabel;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.Label SexLabel;
+        private System.Windows.Forms.ComboBox SexComboBox;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Button RegisterButton;
+    }
+}
